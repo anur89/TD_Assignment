a1bc061ea623541e778a2c3ddf17e432618d6aaf
Sreekumar Sasidharan <anupamarajeswari166@gmail.com>
2019-04-01 13:52:00 -0400

    New assignment Files

---
+++ script.js
@@ -0,0 +1,9 @@
+//$(document).ready(function(){
+    function showProvince() {
+        $('.province_list_items').toggle();
+    }
+
+    function showLanguage() {
+        $('.language_list_items').toggle();
+    }
+//});
